31334aa199f1458eda9e2c963ead996e4ef106c5
Rui <872715188@qq.com>
2022-01-18 23:03:30 +0800

    Ajax éget post

--- once-again-css/js-03-ajax/js/express.js
+++ once-again-css/js-03-ajax/js/express.js
@@ -5,12 +5,40 @@ const express = require('express');
 const app = express();
 
 // 创建路由规则
-app.get('/', (req, resp)=>{
+// get 请求
+app.get('/server', (req, resp)=>{
+    // 设置响应头 设置允许跨域
+    resp.setHeader('Access-Control-Allow-Origin', '*');
     // 设置响应
-    resp.send('HELLO EXPRESS');
+    resp.send('Hello GET');
+
+});
+
+app.post('/server', (req, resp) => {
+    // 设置响应头 设置允许跨域
+    resp.setHeader('Access-Control-Allow-Origin', '*');
+    // 允许所有类型的头
+    resp.setHeader('Access-Control-Allow-Headers', '*');
+    // 设置响应
+    resp.send('Hello POST');
+});
+
+app.all('/json-server', (req, resp) => {
+    // 设置响应头 设置允许跨域
+    resp.setHeader('Access-Control-Allow-Origin', '*');
+    // 允许所有类型的头
+    resp.setHeader('Access-Control-Allow-Headers', '*');
+    // 设置响应数据
+    const data = {
+        name: 'RUI',
+        sex: 'male'
+    }
+    let str = JSON.stringify(data);
+    // 设置响应
+    resp.send(str);
 });
 
 // 监听端口
 app.listen(8000, ()=>{
     console.log('服务已启动，8000 端口监听中');
-})
+});
