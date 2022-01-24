46152a5e2a58cd6af80f952f1480e4a39cb2b745
rui <azernia@foxmail.com>
2022-01-24 16:17:41 +0800

    vue2-cli

--- vue/vue2-cli/01_src_分析脚手架/main.js
+++ vue/vue2-cli/01_src_分析脚手架/main.js
@@ -1,18 +1,16 @@
-/*
-  该文件是整个项目的入口文件
- */
+/* 
+	该文件是整个项目的入口文件
+*/
+//引入Vue
 import Vue from 'vue'
-/*
-  所有组件的父组件
- */
+//引入App组件，它是所有组件的父组件
 import App from './App.vue'
-
-// 关闭 Vue 的生产提示
+//关闭vue的生产提示
 Vue.config.productionTip = false
 
-/*
+/* 
 	关于不同版本的Vue：
-
+	
 		1.vue.js与vue.runtime.xxx.js的区别：
 				(1).vue.js是完整版的Vue，包含：核心功能+模板解析器。
 				(2).vue.runtime.xxx.js是运行版的Vue，只包含：核心功能；没有模板解析器。
@@ -21,7 +19,13 @@ Vue.config.productionTip = false
 			render函数接收到的createElement函数去指定具体内容。
 */
 
+//创建Vue实例对象---vm
 new Vue({
-  //render函数完成了这个功能：将App组件放入容器中
+	el:'#app',
+	//render函数完成了这个功能：将App组件放入容器中
   render: h => h(App),
-}).$mount('#app')
+	// render:q=> q('h1','你好啊')
+
+	// template:`<h1>你好啊</h1>`,
+	// components:{App},
+})
