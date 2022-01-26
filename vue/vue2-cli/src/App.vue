b7bcd8dfe31876ceb84f12cd03c27aa1e6e7f420
Rui <872715188@qq.com>
2022-01-26 22:19:12 +0800

    插槽

--- vue/vue2-cli/src/App.vue
+++ vue/vue2-cli/src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="container">
-    <Search/>
-    <ResultList/>
+    <Category title="游戏">
+      <template scope="data">
+        <ul>
+            <li v-for="(game, index) in data.games" :key="index">
+                {{game}}
+            </li>
+        </ul>
+      </template>
+    </Category>
+    <Category title="游戏">
+      <template scope="data">
+        <ol>
+            <li v-for="(game, index) in data.games" :key="index">
+                {{game}}
+            </li>
+        </ol>
+      </template>
+    </Category>
+    <Category title="游戏">
+      <template scope="{games}">
+        <h4 v-for="(game, index) in games" :key="index">{{game}}</h4>
+      </template>
+    </Category>
   </div>
 </template>
 
 <script>
-import Search from "@/components/Search";
-import ResultList from "@/components/ResultList";
+
+import Category from '@/components/Category'
 
 export default {
   name: "App",
   components: {
-    Search,
-    ResultList
+    Category
   }
 }
 </script>
+
+<style lang="css">
+  .container {
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
---
+++ vue/vue2-cli/src/components/Category.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="category">
+      <h3>{{title}}</h3>
+      <slot :games="games">暂无数据</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props: ['title'],
+    data() {
+        return {
+        games: [
+            '红色警戒',
+            '穿越火线',
+            '劲舞团',
+            '超级马里奥'
+        ]
+        }
+    }
+}
+</script>
+
+<style>
+
+    .category {
+        background-color: skyblue;
+        width: 200px;
+        height: 300px;
+    }
+
+    h3 {
+        text-align: center;
+        background-color: orange;
+    }
+
+    img {
+        width: 100%;
+    }
+</style>
