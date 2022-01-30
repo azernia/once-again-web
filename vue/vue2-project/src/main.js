2a0a603ae4af4fdbd0de34d00b8a67ad8c8dd86c
rui <azernia@foxmail.com>
2022-01-30 23:32:51 +0800

    Home 静态页面

--- vue/vue2-project/src/main.js
+++ vue/vue2-project/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 // 引入路由
 import router from "@/router";
 
+import TypeNav from "@/pages/Home/TypeNav";
+
 Vue.config.productionTip = false
+
+// 注册全局组件
+Vue.component(TypeNav.name, TypeNav);
 
 new Vue({
   render: h => h(App),
--- vue/vue2-project/src/pages/Home/index.vue
+++ vue/vue2-project/src/pages/Home/index.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        首页
+    <!-- 三级联动 -->
+      <TypeNav/>
+      <ListContainer/>
+      <TodayRecommend/>
+      <Rank/>
+      <Like/>
+      <Floor/>
+      <Floor/>
+      <Brand/>
     </div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer";
+import TodayRecommend from "@/pages/Home/TodayRecommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
 export default {
-    name: 'index'
+  name: 'index',
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
