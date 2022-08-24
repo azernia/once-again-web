cbf538e55b99dc3019742f806d17e446aa3a3f65
rui <azernia@foxmail.com>
2022-08-24 16:00:09 +0800

    feat: toRef toRefs

---
+++ vue3/vue3-js/src-toRef-toRefs/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="isShowTest = !isShowTest">change</button>
+  <Test v-if="isShowTest"/>
+</template>
+
+<script>
+import {ref} from "vue";
+import Test from "@/components/Test";
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  },
+  setup() {
+    const isShowTest = ref(true);
+
+    return {
+      isShowTest
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
