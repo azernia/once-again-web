08047c36d2a9e877a5ed821a9188faec3d59b962
rui <azernia@foxmail.com>
2022-08-24 14:48:48 +0800

    feat: watchEffect

---
+++ vue3/vue3-js/src-watchEffect/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Test/>
+</template>
+
+<script>
+import Test from "@/components/Test";
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+
+</style>
