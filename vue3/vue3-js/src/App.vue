c19167e552c04a9da0ffae1001b9d223dfbb4df7
rui <azernia@foxmail.com>
2022-08-24 17:27:58 +0800

    feat: single component

--- vue3/vue3-js/src/App.vue
+++ vue3/vue3-js/src/App.vue
@@ -1,36 +1,19 @@
 <template>
-  <button @click="isShowTest = !isShowTest">change</button>
-  <Suspense>
-    <!-- 真正展示 -->
-    <template v-slot:default>
-      <Test v-if="isShowTest"/>
-    </template>
-    <!-- 加载不出 -->
-    <template v-slot:fallback>
-      Loading...
-    </template>
-  </Suspense>
+  <h1>{{a}}</h1>
+  <SingleComponent a="123" @modify="modifyA"/>
 </template>
 
-<script>
-import {ref, defineAsyncComponent} from "vue";
-// import Test from "@/components/Test";
+<script setup>
+import {ref} from "vue";
+import SingleComponent from './components/SingleComponent.vue';
 
-const Test = defineAsyncComponent(() => import('./components/Test')); // 异步引入
+const a = ref(0);
 
-export default {
-  name: 'App',
-  components: {
-    Test
-  },
-  setup() {
-    const isShowTest = ref(true);
-
-    return {
-      isShowTest
-    }
-  }
+const modifyA = (value) => {
+  a.value = value
 }
+
+
 </script>
 
 <style>
