0b1ff9bf80d36ce116581dbf3da59e9ef8ddd0a9
Rui <872715188@qq.com>
2022-08-25 23:27:30 +0800

    feat: add eslint config

--- vue3/vue3-vite-ts/.eslintrc.cjs
+++ vue3/vue3-vite-ts/.eslintrc.cjs
@@ -10,5 +10,18 @@ module.exports = {
   ],
   "parserOptions": {
     "ecmaVersion": "latest"
+  },
+  "rules": {
+    "no-invalid-this": "warn",
+    "init-declarations": "warn",
+    "array-bracket-newline": "warn",
+    "array-bracket-spacing": "warn",
+    "comma-dangle": "error",
+    "comma-spacing": "warn",
+    "comma-style": ["error", "last"],
+    "keyword-spacing": "warn",
+    "key-spacing": "warn",
+    "arrow-spacing": "warn",
+    "semi": ["error", "always"]
   }
 }
